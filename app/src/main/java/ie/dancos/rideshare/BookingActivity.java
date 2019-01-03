63f93dc4425ddd4ccf31f4005c35af524a571158
dan <dancos.dev@gmail.com>
2019-01-03 22:22:14 +0000

    Delete old Login and Register activities

--- app/src/main/java/ie/dancos/rideshare/BookingActivity.java
+++ app/src/main/java/ie/dancos/rideshare/BookingActivity.java
@@ -1,7 +1,6 @@
 package ie.dancos.rideshare;
 
-import android.app.Activity;
-import android.content.Intent;
+ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
